a7913a02a983daf714127eb2bdaad0c6be9feba1
Ryo Nakamine <rnakamine8080@gmail.com>
2021-05-05 16:54:53 +0900

    Add BaseClient

--- main.go
+++ main.go
@@ -1,7 +1,53 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/Azure/azure-sdk-for-go/services/keyvault/v7.1/keyvault"
+	"github.com/Azure/go-autorest/autorest/azure"
+	"github.com/Azure/go-autorest/autorest/azure/auth"
+)
+
+type AzureKeyVaultCertificate struct {
+	Ctx       context.Context
+	VaultName string
+
+	Client keyvault.BaseClient
+
+	authenticated bool
+	vaultBaseURL  string
+}
+
+func (akv *AzureKeyVaultCertificate) GetKeyVaultClient() (err error) {
+	akv.Client = keyvault.New()
+	authorizer, err := auth.NewAuthorizerFromEnvironment()
+	if err != nil {
+		return err
+	}
+
+	akv.Client.Authorizer = authorizer
+	akv.authenticated = true
+
+	akv.vaultBaseURL = fmt.Sprintf("https://%s.%s", akv.VaultName, azure.PublicCloud.KeyVaultDNSSuffix)
+
+	return nil
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	vaultName := os.Getenv("VAULT_NAME")
+	// certificateName := os.Getenv("CERTIFICATE_NAME")
+
+	ctx := context.Background()
+
+	certificate := AzureKeyVaultCertificate{
+		Ctx:       ctx,
+		VaultName: vaultName,
+	}
+
+	if err := certificate.GetKeyVaultClient(); err != nil {
+		fmt.Println("Error", err)
+		return
+	}
 }
